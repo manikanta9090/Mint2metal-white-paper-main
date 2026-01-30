ff1883ba36bc645c2beb96e3ae0a2006a9159b01
Manikanta <juttumanikanta52@gmail.com>
2026-01-30 13:36:26 +0500

    Fix port for Render

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // Use PORT from .env (fallback 8000)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
